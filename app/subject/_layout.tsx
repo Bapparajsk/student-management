c2b367ba3c6520da6d2b90513b88fa17af5486cb
Bapparaj sk <bapparajsk97@gmail.com>
2026-06-17 22:56:37 +0530

    feat: enhance BottomTabBar with external tab support; integrate icon and navigation for improved user experience

--- app/subject/_layout.tsx
+++ app/subject/_layout.tsx
@@ -1,3 +1,5 @@
+import { colors } from '@/utils/theme';
+import { SimpleLineIcons } from '@expo/vector-icons';
 import BottomTabBar from 'components/bottomTabBar/TabBar';
 import { Tabs } from "expo-router";
 
@@ -5,7 +7,12 @@ export default function SubjectLayout() {
 
     return (
         <Tabs
-            tabBar={(props) => <BottomTabBar {...props} />}
+            tabBar={(props) => (
+                <BottomTabBar
+                    externalTab={{ href: "/(tab)", name: "Overview", Icon: <SimpleLineIcons name="graph" size={22} color={colors.textSecondary} /> }}
+                    {...props}
+                />
+            )}
             screenOptions={{ headerShown: false, animation: "fade" }}
         >
             <Tabs.Screen name="index" options={{ title: "Subject", }} />
--- components/bottomTabBar/TabBar.tsx
+++ components/bottomTabBar/TabBar.tsx
@@ -1,22 +1,32 @@
 import { cn } from '@/utils/ch';
 import { colors } from '@/utils/theme';
-import { SimpleLineIcons } from '@expo/vector-icons';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
+import { Href, useRouter } from "expo-router";
 import { TouchableOpacity, View } from 'react-native';
 import Animated, { useAnimatedStyle, withTiming } from "react-native-reanimated";
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import ThemeText from '../ui/ThemeText';
 import Tab from "./Tab";
 
-export default function BottomTabBar({ state, descriptors, navigation }: BottomTabBarProps) {
+export type CustomBottomTabBarProps = BottomTabBarProps & {
+    externalTab?: {
+        Icon: any;
+        name: string;
+        href: Href;
+    }
+};
+
+export default function BottomTabBar({ state, descriptors, navigation, externalTab }: CustomBottomTabBarProps) {
 
     const insets = useSafeAreaInsets();
+    const router = useRouter();
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
             bottom: withTiming(15 + Math.round(insets.bottom)),
         }
-    })
+    });
+
 
     return (
         <Animated.View
@@ -29,25 +39,27 @@ export default function BottomTabBar({ state, descriptors, navigation }: BottomT
 
             }, animatedStyle]}
         >
-            <TouchableOpacity
-                activeOpacity={0.8}
-                className={'w-20 h-14 rounded-full items-center justify-center overflow-hidden mr-2 border border-white/8 bg-[rgba(17,24,39,0.85)]'}
-            >
-                <SimpleLineIcons
-                    name="graph"
-                    size={22}
-                    color={colors.textMuted}
-                />
-
-                <ThemeText
-                    className={cn(
-                        'text-xs mt-0.5 font-poppins-semibold',
-                    )}
-                    style={{ color: colors.textMuted }}
+            {externalTab && (
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    className={'w-20 h-14 rounded-full items-center justify-center overflow-hidden mr-2 border border-white/8'}
+                    style={{
+                        backgroundColor: "rgba(17,24,39,0.85)",
+                    }}
+                    onPress={() => {
+                        router.dismissTo(externalTab.href);
+                    }}
                 >
-                    Overview
-                </ThemeText>
-            </TouchableOpacity>
+                    {externalTab.Icon}
+                    <ThemeText
+                        className={cn(
+                            'text-xs mt-0.5 font-poppins-semibold',
+                        )}
+                        style={{ color: colors.textSecondary }}
+                    >
+                        {externalTab.name}
+                    </ThemeText>
+                </TouchableOpacity>)}
             <View
                 style={[{
                     alignSelf: 'center',
@@ -74,7 +86,6 @@ export default function BottomTabBar({ state, descriptors, navigation }: BottomT
                     />
                 ))}
             </View>
-
         </Animated.View>
 
     )
